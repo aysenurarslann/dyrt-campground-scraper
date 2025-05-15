6566415df1e7f5d6a67cba1267af7a37ca422fc5
Ayşenur Arslan <92011761+aysenurarslann@users.noreply.github.com>
2025-05-16 00:13:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 #main.py
-"""
-Main entrypoint for The Dyrt web scraper case study.
 
-Usage:
-    The scraper can be run directly (`python main.py`) or via Docker Compose (`docker compose up`).
-
-If you have any questions in mind you can connect to me directly via info@smart-maple.com
-"""
 import argparse
 
 import asyncio
